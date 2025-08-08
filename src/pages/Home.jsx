f08529b82b69b1ceb8da13a29c1fd82a43c1c2fc
Munna-Scripts <munnascriptz@gmail.com>
2025-08-08 14:34:43 +0600

    teaches Complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import Teaches from '../components/Teaches'
 
 const Home = () => {
   return (
     <>
       <Banner/>
+      <Teaches/>
     </>
   )
 }
