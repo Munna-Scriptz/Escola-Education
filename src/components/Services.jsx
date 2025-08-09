28223e28c78133eae445c91003a1a4206a5d43f1
Munna-Scripts <munnascriptz@gmail.com>
2025-08-09 12:27:03 +0600

    Service Slider Cart Pfp added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import CommonHead from './common/CommonHead'
+import pfp from '../assets/images/ServicesProfile.png'
+import { FaRegUser } from "react-icons/fa6";
 
 const Services = () => {
   return (
@@ -9,6 +11,19 @@ const Services = () => {
                 {/* -------------Header----------------- */}
                 <div className='w-[434px]'>
                     <CommonHead topText={'Services we’re offering'} BottomText={'Navigating your path to success'}/>
+                </div>
+                {/* -------------Services Slider----------------- */}
+                <div className='mt-[40px] w-[410px] p-[30px]'>
+                    <div className='flex items-center justify-between'>
+                        <div className='flex items-center gap-[10px]'>
+                            <img src={pfp} alt="Review Imag" />
+                            <p className='text-secondBK font-inter text-[13px]'>By leonardo</p>
+                        </div>
+                        <div className='flex items-center gap-[10px]'>
+                            <FaRegUser className='text-[#2F57EF]'/>
+                            <p className='text-secondBK font-inter text-[13px]'>50 Students</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
